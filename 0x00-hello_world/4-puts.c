ad29c84bc94144b79a73731ac2ff91716c14265b
Moseskamau <kibem323@gmail.com>
2023-07-13 09:35:12 -0700

    4-puts commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
 /**
- * main - Entry point
- *
- * Return: Always 0 (Sucess)
+ *main - A program that prints a line with puts function
+ *Return : Always 0 (success)
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle
-");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
 
