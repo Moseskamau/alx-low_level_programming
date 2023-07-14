69653dd48b375be6109eef9dac52997754d567f6
Moseskamau <kibem323@gmail.com>
2023-07-14 07:00:58 -0700

    6-size.c commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,9 +12,9 @@ int main(void)
 	float f;
 
 printf("Size of a char: %1u byte(s)\n", (unsigned long)sizeof(a));
-printf("Size of an int: %1u byte(s)\n", (unsigned long)sizeof(b));
-printf("Size of a long int: %1u byte(s)\n", (unsigned long)sizeof(c));
-printf("Size of a long int: %1u byte(s)\n", (unsigned long)sizeof(d));
-printf("Size of a float: %1u byte(s)\n", (unsigned long)sizeof(f));
+printf("Size of an int: %4u byte(s)\n", (unsigned long)sizeof(b));
+printf("Size of a long int: %4u byte(s)\n", (unsigned long)sizeof(c));
+printf("Size of a long int: %8u byte(s)\n", (unsigned long)sizeof(d));
+printf("Size of a float: %4u byte(s)\n", (unsigned long)sizeof(f));
 return (0);
 }
