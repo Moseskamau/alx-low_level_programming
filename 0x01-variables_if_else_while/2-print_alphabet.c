0bb6ac63207362adc721e40c0b8b95824620a018
Moseskamau <kibem323@gmail.com>
2023-07-17 11:24:20 -0700

    print alphabet commit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ *  main - Entry point
+ *  Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char z;
+
+	z = 'a';
+	while
+		(z <= 'm') {
+			putchar(z);
+			z++;
+		}
+	putchar('\n');
+	return (0)
+}	
